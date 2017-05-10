20b78bf2cd2cd8675a11a5af71d742febcc17b6a
Ta Thanh Dinh <tathanhdinh@gmail.com>
2017-05-10 23:55:43 +0200

    continue

---
+++ tracer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stddef.h>
+
+#include "dr_api.h"
+#include "drmgr.h"
+#include "drreg.h"
+#include "drutil.h"
+//#include "utils.h"
+
+
+DR_EXPORT void
+dr_client_main(client_id_t id, int argc, const char *argv[]) {
+
+}
