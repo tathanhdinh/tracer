4576edb75f5d8b3eda0e92c3e2ede2b7d55fe901
Ta Thanh Dinh <tathanhdinh@gmail.com>
2017-05-13 01:38:34 +0200

    continue

--- tracer.c
+++ tracer.c
@@ -7,8 +7,25 @@
 #include "drutil.h"
 //#include "utils.h"
 
+void event_thread_init(void *drcontext);
 
 DR_EXPORT void
 dr_client_main(client_id_t id, int argc, const char *argv[]) {
+    drreg_options_t reg_ops = { sizeof (reg_ops), 3, false };
+    if (drreg_init(&ops) != DRREG_SUCCESS) {
+        DR_ASSERT(false);
+        return;
+    }
+
+    if (!drmgr_register_thread_init_event(event_thread_init)) {
+        DR_ASSERT(false);
+        return;
+    }
+
+}
+
+
+static void
+event_thread_init(void *drcontext) {
 
 }
